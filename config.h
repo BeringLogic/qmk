0184212889a01c0e7202fc68eb61d3b0f6b7c36b
Philippe Gouin <bering@ringlogic.com>
2025-08-20 17:25:20 -0400

    adjusted TAPPING_TERM and removed FLOW_TAP_TERM

--- config.h
+++ config.h
@@ -43,7 +43,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
-#define FLOW_TAP_TERM 150
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 175
 
 #define ENABLE_RGB_MATRIX_SOLID_COLOR
 #define RGB_MATRIX_DEFAULT_HUE 30
